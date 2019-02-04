6058aec6546954e792c0875542982464b55ca819
dmitri-mcguckin <dmitri.mcguckin26@gmail.com>
2019-02-04 14:26:34 -0800

    anonymous upate

--- pch.h
+++ pch.h
@@ -12,7 +12,6 @@ using namespace std;
 struct boardSpace {
 	char terrain;
 	char extThings;
-	char unknownBlank;
 };
 
 
@@ -24,17 +23,17 @@ class Board {
             void showTerrain();		// user can display all the board
             void showALL();			// can give up and see everything
             ~Board();
-            
+
         private:
-            void setBoard();					// randomly sets up board. 
+            void setBoard();					// randomly sets up board.
             int randomOutput();					// generates a random number
-            char fillSpaces(int rndInput);		// fills the matrix with terrain 
-            char fillExternals(int rndInput);	// fills the matrix with extras 
+            char fillSpaces(int rndInput);		// fills the matrix with terrain
+            char fillExternals(int rndInput);	// fills the matrix with extras
 
 
-            boardSpace** gameSpace;  // struct that has 3 qualities. 
+            boardSpace** gameSpace;  // struct that has 3 qualities.
                                      // blank space for user to uncover
-                                     // space with terrain 
+                                     // space with terrain
                                      // space with the extra thing like food, jewels, etc
             int userInput;			 // variable for user input on matrix size
 };
