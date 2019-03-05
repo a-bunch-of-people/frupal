86e7405ae3a55ab9d8348de83fd35a0628864638
Bradley Thompson <bradlet2@babbage.cs.pdx.edu>
2019-03-04 16:13:48 -0800

    Built up the equipment base class

--- equipment/equipment.h
+++ equipment/equipment.h
@@ -1,13 +1,15 @@
 //Dev: Bradley Thompson
 
 #include <iostream>
+#include <cstring>
 
 //This is the home for the equipment hierarchy.
 //I'll have operators, copy constructors, blah blah,
 //so that hopefully the equipment classes are really 
 //easy to use.
 
-
+#define MAX_STRSIZE 50
+#define MAX_DURBL 100
 
 /* Equipment class -- Base class */
 class equipment
@@ -15,9 +17,18 @@ class equipment
   public:
     equipment(); ~equipment();
     equipment(const equipment& copy);
+    
+    int fill_equipment(void);         //User input to fill an empty object.
+    void get_name(char* new_name);  //arg data filling functions
+    void get_durbl(int new_durbl);    //These will overwrite existing data.
+    void get_dmg(int new_dmg);
+    void report(void);                //Prints equip info to standard out
+
+    //Operators
+    equipment& operator=(const equipment& copy);
 
   private:
     char* name;
-    int durabilty;
+    int durability;
     int damage;
 };
